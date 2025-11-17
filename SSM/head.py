2f23b92115be9308165440876649ef9871a7f793
dan1oo <wangda2022@gmail.com>
2025-11-16 23:12:54 -0600

    Model code

--- SSM/head.py
+++
@@ -1,7 +0,0 @@
-
-import numpy as np
-# The main class file for the SSM Head
-
-class SSM_Head(SSM_Params):
-
-    def __init__():
---
+++ SSM/hippo.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def hippo_legs(N: int) -> np.ndarray:
+    """
+    Construct the N x N HiPPO-LegS continuous-time operator.
+
+    This is the operator used in S4 to approximate online Legendre
+    projection. Here we use the standard dense definition:
+
+        A_{n,m} =
+            -sqrt((2n+1)(2m+1))   if n > m
+            -(2n+1)               if n = m
+            0                     if n < m
+    """
+    A = np.zeros((N, N))
+    for n in range(N):
+        for m in range(N):
+            if n > m:
+                A[n, m] = -np.sqrt((2 * n + 1) * (2 * m + 1))
+            elif n == m:
+                A[n, m] = -(2 * n + 1)
+            # else: A[n, m] = 0
+    return A
+
+
+def discretize_bilinear(A_ct: np.ndarray, dt: float = 1.0) -> np.ndarray:
+    """
+    Bilinear (Tustin) discretization:
+
+        A_d = (I - dt/2 * A_ct)^(-1) (I + dt/2 * A_ct)
+
+    This is the transform used in S4 to go from continuous-time
+    to discrete-time SSM parameters.
+    """
+    N = A_ct.shape[0]
+    I = np.eye(N)
+    left = I - 0.5 * dt * A_ct
+    right = I + 0.5 * dt * A_ct
+    A_d = np.linalg.inv(left) @ right
+    return A_d
